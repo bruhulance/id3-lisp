44ad78e63301e556683753d2cfa594a65188b878
bruhulance <liamm91@gmail.com>
2022-07-03 21:28:31 -0400

    did stuff

--- src/utils.lisp
+++ src/utils.lisp
@@ -21,6 +21,7 @@
       result)))
 
 (defun collect-id3-tag (stream)
+  (declare (stream stream))
   "this function assumes that the stream has already read the ID3 of the ID3 header"
   (append
    '(73 68 51)
@@ -28,19 +29,23 @@
 	 repeat 7)))
 
 (defmacro id3-head? (stream)
+  (declare (stream stream))
   "this function reads two more bytes from a byte stream to determine whether or not there is an ID3 tag"
-  `(progn (let* ((b1 (read-byte ,stream nil 'eof))
-		 (b2 (read-byte ,stream nil 'eof)))
-	    (and ,@(mapcar #'(lambda (x y)
-			       `(= ,x (char-code ,y)))
-			   '(b1 b2)
-			   '(#\D #\3))))))
+  `(the boolean
+	(progn (let* ((b1 (read-byte ,stream nil 'eof))
+		      (b2 (read-byte ,stream nil 'eof)))
+		 (and ,@(mapcar #'(lambda (x y)
+				    `(= ,x (char-code ,y)))
+				'(b1 b2)
+				'(#\D #\3)))))))
 
 (defun as-keyword (sym)
+  (declare (symbol sym))
   "converts a symbol to a keyword"
   (intern (string sym) :keyword))
 
 (defun slot->defclass-slot (name)
+  (declare (symbol name))
   "helper function to create a class"
   `(,name :initarg ,(as-keyword name) :accessor name))
 
@@ -49,17 +54,25 @@
   `(defclass ,name ()
      ,(mapcar #'slot->defclass-slot slots)))
 
-(defun id3-class ()
+(defun define-id3-class ()
   "this function uses a macro to create a class"
   (define-binary-class id3-tag
       (identifier major-version revision flags size frames)))
 
-(defmacro take-from (list &rest indices)
+(defun take-from (list &rest indices)
+  (declare (list list indices))
   "this function takes the indices from a list and puts them into a new list"
-  `(list ,@(mapcar #'(lambda (x) `(nth ,x ,list)) indices)))
+  `(list ,@(mapcar #'(lambda (x) `(nth ,x ,list)) indices)))i
 
-(defun id3-header->class (tag)
-  "this function converts a tag (list) to a class"
+(defgeneric data->id3-tag (tag)
+  (:documentation "this function turns data into a proper id3 tag class"))
+
+(defmethod data->id3-tag ((tag stream))
+  "reading from a stream to create an id3-tag class"
+  t)
+
+(defmethod data->id3-tag ((tag cons))
+  "this function converts a cons cell to a id3-tag"
   (let ((object (make-instance 'id3-tag)))
     (with-slots (identifier major-version revision flags size frames) object
       (setf identifier (take-from tag 0 1 2))
